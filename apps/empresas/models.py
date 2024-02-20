3bf893ceda5f3d70fb4931c9dcab75f9acff26e8
Cassio Junior <jrrrtricolor@outlook.com>
2024-02-20 23:09:57 +0000

    Criando o relacionamentos do banco de dados

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text="Nome do empresa")
 
+    def __str__(self):
+        return self.nome
 
 
